3e56ee5698cfd17a243bfae0c24d2500202ac80c
Mallo <barataymallory@gmail.com>
2026-02-03 17:03:14 +0100

    ex3 ajout string

--- app/src/main/java/com/example/formulaire/MainActivity.java
+++ app/src/main/java/com/example/formulaire/MainActivity.java
@@ -1,17 +1,11 @@
     package com.example.formulaire;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText etNom = (EditText) findViewById(R.id.editTextNom);
-        EditText etPrenom = (EditText) findViewById(R.id.editTextPrenom);
-        EditText etAge = (EditText) findViewById(R.id.editTextAge);
-        EditText etDomComp = (EditText) findViewById(R.id.editTextDom);
-        EditText etNumTel = (EditText) findViewById(R.id.editTextPhone2);
-        Button butSubmit = (Button) findViewById(R.id.button);
+        EditText etNom = findViewById(R.id.editTextNom);
+        EditText etPrenom = findViewById(R.id.editTextPrenom);
+        EditText etAge = findViewById(R.id.editTextAge);
+        EditText etDomComp = findViewById(R.id.editTextDom);
+        EditText etNumTel = findViewById(R.id.editTextPhone2);
+        Button butSubmit = findViewById(R.id.button);
 
-        butSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(),"Bonjour "+etPrenom.getText()+" "+etNom.getText(), Toast.LENGTH_LONG).show();
-            }
-        });
+        butSubmit.setOnClickListener(arg0 -> Toast.makeText(getApplicationContext(),
+                "Bonjour "+etPrenom.getText()+" "+etNom.getText(), Toast.LENGTH_LONG).show());
     }
 }
