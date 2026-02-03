25c7f889204096826b6faf399627c1509c679bb3
Mallo <barataymallory@gmail.com>
2026-02-03 16:39:43 +0100

    ex3bis Fin

--- app/src/main/java/com/example/formulaire/MainActivity.java
+++ app/src/main/java/com/example/formulaire/MainActivity.java
@@ -1,36 +1,57 @@
     package com.example.formulaire;
 
+import static android.widget.LinearLayout.VERTICAL;
+
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EditText etNom = (EditText) findViewById(R.id.editTextNom);
-        EditText etPrenom = (EditText) findViewById(R.id.editTextPrenom);
-        EditText etAge = (EditText) findViewById(R.id.editTextAge);
-        EditText etDomComp = (EditText) findViewById(R.id.editTextDom);
-        EditText etNumTel = (EditText) findViewById(R.id.editTextPhone2);
-        Button butSubmit = (Button) findViewById(R.id.button);
+        LinearLayout.LayoutParams params =
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+        LinearLayout linearLayout = new LinearLayout(this);
+        EditText etNom = new EditText(this);
+        EditText etPrenom = new EditText(this);
+        EditText etAge = new EditText(this);
+        EditText etDomComp = new EditText(this);
+        EditText etNumTel = new EditText(this);
+        Button butSubmit = new Button(this);
+        etNom.setHint("Nom");
+        etNom.setLayoutParams(params);
+        etPrenom.setHint("Prenom");
+        etPrenom.setLayoutParams(params);
+        etAge.setHint("Age");
+        etAge.setLayoutParams(params);
+        etDomComp.setHint("Domaine de compétence");
+        etDomComp.setLayoutParams(params);
+        etNumTel.setHint("Numéro de téléphone");
+        etNumTel.setLayoutParams(params);
+        butSubmit.setHint("Submit");
+        butSubmit.setLayoutParams(params);
 
-        butSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(),"Bonjour "+etPrenom.getText()+" "+etNom.getText(), Toast.LENGTH_LONG).show();
-            }
-        });
+
+        linearLayout.addView(etNom);
+        linearLayout.addView(etPrenom);
+        linearLayout.addView(etAge);
+        linearLayout.addView(etDomComp);
+        linearLayout.addView(etNumTel);
+        linearLayout.addView(butSubmit);
+
+        linearLayout.setOrientation(VERTICAL);
+
+        butSubmit.setOnClickListener(arg0 -> Toast.makeText(getApplicationContext(),
+                "Bonjour "+etPrenom.getText()+" "+etNom.getText(), Toast.LENGTH_LONG).show());
+        setContentView(linearLayout);
     }
 }
