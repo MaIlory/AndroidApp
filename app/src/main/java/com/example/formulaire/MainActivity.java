00a267575ae644a3748a8b2554de564ea3494328
Mallo <barataymallory@gmail.com>
2026-02-10 14:11:20 +0100

    Ajout Boite de dialog pour confirmation

--- app/src/main/java/com/example/formulaire/MainActivity.java
+++ app/src/main/java/com/example/formulaire/MainActivity.java
@@ -1,6 +1,9 @@
     package com.example.formulaire;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,7 +23,32 @@ public class MainActivity extends AppCompatActivity {
         EditText etNumTel = findViewById(R.id.editTextPhone2);
         Button butSubmit = findViewById(R.id.button);
 
-        butSubmit.setOnClickListener(arg0 -> Toast.makeText(getApplicationContext(),
-                "Bonjour "+etPrenom.getText()+" "+etNom.getText(), Toast.LENGTH_LONG).show());
+        butSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.attention);
+                builder.setMessage(R.string.voulez_vous_continuer);
+
+                builder.setPositiveButton(R.string.continuer, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Action si OUI
+                        Toast.makeText(MainActivity.this, "Vous avez cliqué sur Continuer", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.dismiss, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
